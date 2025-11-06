c8ca71fa0d0450a278ca101c7e5ddab91a9502c9
sem1n <songsm1129@gmail.com>
2025-11-06 13:21:59 +0900

    First Commit

---
+++ src/main/kotlin/kr/ac/kumoh/s20240624/w25w08_thymeleaf/controller/SongController.kt
@@ -0,0 +1,12 @@
+package kr.ac.kumoh.s20240624.w25w08_thymeleaf.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class SongController {
+    @GetMapping("/songs/random")
+    fun getRandomSong(): String {
+        return "random"
+    }
+}
